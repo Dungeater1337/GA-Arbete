95bbb00da978d0ee4dcd80144436f84e1d716f1a
AdamAkhooo <111962656+Dungeater1337@users.noreply.github.com>
2024-03-11 12:41:56 +0100

    dsdsd
    sdsdsdsd

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
-let userArray = []
+let userArray = [];
+let indexMain;
 
 function init(){
+    indexMain = document.getElementById("IndexMain");
+    console.log(indexMain);
     GetUsers();
 }
 window.onload = init();
@@ -23,4 +26,51 @@ async function GetUsersFetch(){
 async function GetUsers(){
     userArray = await GetUsersFetch();
     console.log(userArray);
+
+    userArray.forEach(element=>{
+       createSection(element);
+    })
+}
+
+function createSection(el){
+    let section = document.createElement("section");
+
+    let figure = createFigure(el);
+    section.appendChild(figure);
+    console.log(section);
+    indexMain.appendChild(section);
+}
+
+function createFigure(el){
+    let figElem = document.createElement("figure");
+
+    let img = createImage(el);
+    let div = createDiv(el);
+
+    figElem.appendChild(img);
+    figElem.appendChild(div);
+    console.log(figElem);
+
+
+
+    return figElem;
+}
+
+function createImage(el){
+    let imgElem = document.createElement("img");
+    imgElem.setAttribute("src", el.image);
+    imgElem.setAttribute("alt", el.title);
+    return imgElem;
+}
+
+function createDiv(el){
+    let div = document.createElement("div");
+    div.setAttribute("class", "text-overlay");
+
+    let header = document.createElement("h3");
+    header.innerHTML = el.title;
+
+    div.appendChild(header);
+
+    return div;
 }
