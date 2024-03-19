ec7a24563d42a7592f7af48c29d8896083f75d9a
Dungeater1337 <111962656+Dungeater1337@users.noreply.github.com>
2024-03-19 15:49:35 +0100

    sdsds
    dsdsdsdsd

--- javascript.js
+++ javascript.js
@@ -1,11 +1,21 @@
 let userArray = [];
 let indexMain;
 
+
+//list med länkar till review img...
+const imageLinks = [
+    "https://m.media-amazon.com/images/I/6110RSDn3PL.jpg",
+    "https://i.ebayimg.com/images/g/SeAAAOSwcqhfjFwG/s-l1600.jpg",
+    "https://static.displate.com/857x1200/displate/2023-06-12/6e217abc7f5bb5d0dc56e68752193a11_5c51574f5f2f216f9ef25a0d08fa6400.jpg",
+    "https://i0.wp.com/www.gamezawy.com/wp-content/uploads/2023/09/Cyber-Phantom-DLC.jpg"
+];
+
 function init(){
     indexMain = document.getElementById("IndexMain");
     console.log(indexMain);
     GetUsers();
 }
+
 window.onload = init();
 
 async function GetUsersFetch(){ 
@@ -19,7 +29,7 @@ async function GetUsersFetch(){
         }
     });
     console.log(response);
-    userArray = response.json();
+    userArray = await response.json();
     return userArray;
 }
 
@@ -28,6 +38,7 @@ async function GetUsers(){
     console.log(userArray);
 
     userArray.forEach(element=>{
+        element.image = imageLinks[index % imageLinks.lenght];
        createSection(element);
     })
 }
@@ -50,8 +61,6 @@ function createFigure(el){
     figElem.appendChild(img);
     figElem.appendChild(div);
     console.log(figElem);
-
-
 
     return figElem;
 }
